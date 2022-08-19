1f0204f309fe048e50bb274c9753d95918e140e5
Juan Funes <100494946+JuanFunes9@users.noreply.github.com>
2022-08-19 01:15:42 -0300

    Update testimonial_controllers.js

--- controllers/testimonial_controllers.js
+++ controllers/testimonial_controllers.js
@@ -34,7 +34,7 @@ const putTestimonial = async (req, res = resonse) => {
 	if (content) testimonial.conent = content;
 	if (image) testimonial.image = image;
 
-	await Testimonial.save();
+	await testimonial.save();
 	res.status(200).json({ data: testimonial })
 }
 
