43e73cbab38ae41a23cdbd40b36438dc3bd5023f
Gastti <gutigas96@gmail.com>
2022-08-22 20:56:20 -0300

    updated: delete comments

--- controllers/activity_controllers.js
+++ controllers/activity_controllers.js
@@ -1,77 +1,6 @@
 const db = require('../models/index.js');
 const Activity = db.Activity;
 
-// const getAllActivities = async (req, res = response) => {
-//     const activities = await Activity.findAll();
-
-//     res.status(200).json({
-//         ok: true,
-//         data: activities
-//     })
-// }
-
-// const getActivityById = async (req, res = response) => {
-//     const { id } = req.params;
-//     const activity = await Activity.findByPk(id);
-
-//     if (!activity) return res.json({msg: 'Activity not found.'});
-
-//     res.status(200).json({
-//         ok: true,
-//         data: activity
-//     })
-// }
-
-// const postActivity = async (req, res = response) => {
-//     const { name, content, image } = req.body;
-//     const activity = await Activity.create({ name, content, image });
-
-//     res.status(200).json({
-//         ok: true,
-//         data: activity
-//     })
-// }
-
-// const putActivity = async (req, res = response) => {
-//     const { id } = req.params;
-//     const { name, content, image } = req.body;
-
-//     const activity = await Activity.findByPk(id);
-//     if (!activity) return res.json({msg: 'Activity not found.'});
-
-//     if(name) activity.name = name;
-//     if(content) activity.content = content;
-//     if(image) activity.image = image;
-
-//     await activity.save();
-
-//     res.status(200).json({
-//         ok: true,
-//         data: activity
-//     })
-// }
-
-// const deleteActivity = async (req, res = response) => {
-//     const { id } = req.params;
-
-//     const activity = await Activity.findByPk(id);
-
-//     activity.destroy();
-
-//     res.status(200).json({
-//         ok: true,
-//         data: activity
-//     })
-// }
-
-// module.exports = {
-//     getAllActivities,
-//     getActivityById,
-//     postActivity,
-//     putActivity,
-//     deleteActivity
-// }
-
 class ActivityController {
 
     async get(req, res) {
@@ -145,7 +74,7 @@ class ActivityController {
                 exclude: ['deletedAt', 'createdAt', 'updatedAt']
             }
         });
-        
+
         if (!activity) return res.json({ msg: 'Activity not found.' });
 
         activity.destroy();
