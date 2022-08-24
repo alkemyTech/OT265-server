a4f33113567e78d255c6e120269ead932d848d8d
limperatri <lautaro.d.gutierrez@gmail.com>
2022-08-24 12:51:04 -0300

    spelling

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -15,7 +15,7 @@ const generateAccessToken = (user) => {
 //como extraer el rol del usuario ó lo que fuere
 
 const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization']
+    const authHeader = req.headers['Authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (token === null) return res.status(401).send({
         success: false,
