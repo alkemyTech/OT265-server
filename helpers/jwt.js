ac1879c48c6df7a1478ab955d48a49c5e845fb6e
limperatri <lautaro.d.gutierrez@gmail.com>
2022-08-25 13:03:39 -0300

    Refactor jwt.js

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,66 +1,27 @@
 const jwt = require('jsonwebtoken')
 
-// Esta función debe ser incertada en la ruta de login, después de confirmar
-// que la información que venga por body sea correcta.
+const generateAccessToken = (id = '') => {
 
-const generateAccessToken = (user) => {
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '15m',
-    });
-    return { token: "Bearer " + accessToken }
-}
+    return new Promise((resolve, reject) => {
 
-//Este es el middleware para autenticar el usuario y prevenir el acceso a rutas protegidas
-//Va a ser necesario hacer checkeos extras una vez autenticado el token,
-//como extraer el rol del usuario ó lo que fuere
+        const payload = { id };
 
-const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['Authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (token === null) return res.status(401).send({
-        success: false,
-        message: "missing token"
-    })
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.status(403).send({
-            success: false,
-            message: "Wrong credentials"
+        jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: '1h'
+        }, (err, token) => {
+            if (err) {
+                console.log(err);
+                reject('No se pudo generar el token')
+            } else {
+                resolve(token);
+            }
         })
-        req.user = user; //Esta linea está asignando AL USER ENTERO EN {req.user}.
-        //No se que datos son necesarios guardar asi que la dejo asi.
-        next()
-    })
-}
 
-// Este middleware hace lo mismo que el de autenticación pero retorna los datos de el user
-// en caso de ser necesario.
-
-const getUserFromToken = (req, res, next) => {
-    const authHeader = req.headers['Authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (token === null) return res.status(401).send({
-        success: false,
-        message: "missing token"
-    })
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.status(403).send({
-            success: false,
-            message: "Wrong credentials"
-        })
-        return {
-            _id: user.id,
-            role: user.role,
-            usarname: user.username
-        }
     })
 
 }
 
 
 module.exports = {
-    generateAccessToken,
-    authenticateToken,
-    getUserFromToken
+    generateAccessToken
 }
