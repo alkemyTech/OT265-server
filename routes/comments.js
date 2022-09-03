cd533020d7c3b39507eecf3e6fd763a36e3148fb
Gastti <gutigas96@gmail.com>
2022-09-02 22:23:11 -0300

    updated: add post, update and delete endpoints

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
-const Comment = require('../controllers/comment_controllers');
+const { check } = require("express-validator");
+const Comment = require("../controllers/comment_controllers");
 const { isAdmin } = require("../middlewares/isAdmin");
 const isAuthenticated = require("../middlewares/isAuthenticated");
 
@@ -8,5 +9,20 @@ router.get("/", [
     isAuthenticated,
     isAdmin
 ], Comment.get);
+
+router.post("/", [
+    isAuthenticated,
+    check('body', 'You cannot post an empty comment.').not().isEmpty(),
+    check('post_id', 'You must send a post ID.').not().isEmpty,
+    check('user_id', 'You must send a user ID.').not().isEmpty,
+], Comment.create)
+
+router.put("/:comment_id", [
+    isAuthenticated
+], Comment.update)
+
+router.delete("/:comment_id", [
+    isAuthenticated
+], Comment.delete)
 
 module.exports = router;
