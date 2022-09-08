a53241b75b19c68f59a8c4fdc97f242b9c4e5978
Gastti <gutigas96@gmail.com>
2022-09-08 02:00:25 -0300

    update: disable console logs to testings

--- config/config.js
+++ config/config.js
@@ -7,14 +7,16 @@ module.exports = {
         "database":  process.env.DB_NAME,
         "host": process.env.DB_HOST,
         "port": process.env.DB_PORT,
-        "dialect": "mysql"
+        "dialect": "mysql",
+        "logging": false
     },
     "test": {
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_NAME,
         "host": process.env.DB_HOST,
-        "dialect": "mysql"
+        "dialect": "mysql",
+        "logging": false
     },
     "production": {
         "username": process.env.DB_USER,
