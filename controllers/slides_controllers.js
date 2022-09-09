ec1f24e70211357dd3e06972d05e2878c63c6e0f
Lucas Carnero <lucas.carnero@naranja.com>
2022-09-08 22:47:27 -0300

    fix(slides): restores slide's controller functionalities

--- controllers/slides_controllers.js
+++ controllers/slides_controllers.js
@@ -1,41 +1,86 @@
-const db = require('../models/index');
-   
+const db = require("../models/index");
+
 const Slide = db.Slide;
 
 const editSlide = async (req, res) => {
+  const { id } = req.params;
+
+  const { imageUrl, text, order, organizationId } = req.body;
+
+  const slide = await Slide.findByPk(id, {
+    attributes: {
+      exclude: ["createdAt", "updatedAt"],
+    },
+  });
+
+  if (!slide) {
+    return res.status(400).json({
+      ok: false,
+      msg: `No existe el slide con ID: ${id}.`,
+    });
+  }
+
+  if (imageUrl) slide.imageUrl = imageUrl;
+  if (text) slide.text = text;
+  if (order) slide.order = order;
+  if (organizationId) slide.organizationId = organizationId;
+
+  await slide.save();
+
+  res.json({
+    ok: true,
+    data: slide,
+    msg: `El Slide con ID: ${id} ha sido actualizado con exito.`,
+  });
+};
+
+const slideDetails = async (req, res) => {
+  try {
     const { id } = req.params;
+    const findId = await Slide.findByPk(id);
+    if (!findId)
+      return res.status(404).send({
+        success: false,
+        message: `The slide Id: ${id} doesn't exist`,
+      });
+    res.send({
+      success: true,
+      data: findId,
+    });
+  } catch (err) {
+    res.status(400).send({
+      error: true,
+      message: err.message,
+    });
+  }
+};
 
-    const { imageUrl, text, order, organizationId } = req.body;
-    
-    const slide = await Slide.findByPk( id, {
-        attributes: {
-            exclude: [ 'createdAt', 'updatedAt']
-        }
-    } );
+const listarSlides = async (req, res) => {
+  const slides = await Slide.findAll();
+  res.json({ slides });
+};
 
-    if(!slide){
-        return res.status(400).json({
-            ok: false,
-            msg: `No existe el slide con ID: ${ id }.`
-        })
-    }
+const deleteSlide = async (req, res) => {
+  const { id } = req.params;
 
-    if(imageUrl) slide.imageUrl = imageUrl;
-    if (text) slide.text = text;
-    if (order) slide.order = order;
-    if (organizationId) slide.organizationId = organizationId;
+  const slide = await Slide.findByPk(id);
 
-    await slide.save();
-
-    res.json({
-        ok: true,
-        data: slide,
-        msg: `El Slide con ID: ${ id } ha sido actualizado con exito.`
-    })
-}
+  if (!slide) {
+    return res.status(400).json({
+      ok: false,
+      msg: `No existe el slide con ID: ${id}.`,
+    });
+  }
+  slide.destroy();
+  res.json({
+    ok: true,
+    msg: `El Slide con ID: ${id} ha sido eliminado con exito.`,
+  });
+};
 
 module.exports = {
-    editSlide,
-    deleteSlide,
-    listarSlides
-}
+  editSlide,
+  deleteSlide,
+  listarSlides,
+  slideDetails,
+};
--- routes/slides.js
+++ routes/slides.js
@@ -1,29 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 //-------------------Middlewares----------------------//
 
-const { isAdmin } = require('../middlewares/isAdmin');
-const isAuthenticated = require('../middlewares/isAuthenticated');
+const { isAdmin } = require("../middlewares/isAdmin");
+const isAuthenticated = require("../middlewares/isAuthenticated");
 
 //-------------------Controllers----------------------//
-const { listarSlides, deleteSlide, slideDetails, editSlide } = require('../controllers/slides_controllers');
+const {
+  listarSlides,
+  deleteSlide,
+  slideDetails,
+  editSlide,
+} = require("../controllers/slides_controllers");
 
-router.put('/:id', [
-    isAuthenticated,
-    isAdmin
-], editSlide);
+router.put("/:id", [isAuthenticated, isAdmin], editSlide);
 
+router.delete("/:id", [isAuthenticated, isAdmin], deleteSlide);
 
+router.get("/", [isAuthenticated, isAdmin], listarSlides);
 
-
-router.get('/', [
-    isAuthenticated,
-    isAdmin
-], listarSlides);
-
-
-router.get('/:id', slideDetails);
-
-
+router.get("/:id", slideDetails);
 
 module.exports = router;
