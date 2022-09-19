b6531aa939b5c8bb8620133eee637d80fb9e677a
ortega-pablo <p.ortegariera@gmail.com>
2022-09-19 20:56:33 -0300

    feat[test]:tests for authentication routes

--- test/auth.test.js
+++ test/auth.test.js
@@ -18,13 +18,13 @@ const validValues = {
     password: 'passwordTest'
 }
 
+
 describe('Auth Tests', () => {
 
     let id;
     beforeEach(async () => {
         const user = await User.create(validValues);
         id = user.id;
-        adminToken = await generateAccessToken(1);
         userToken = await generateAccessToken(11)
     });
 
@@ -33,12 +33,12 @@ describe('Auth Tests', () => {
         await User.destroy({ where: { id: id + 1 }, force: true });
     });
 
-    // Post method tests
+    // Post  register method tests
     describe('Post register Method', () => {
 
         it("Should register a user", (done) => {
             chai.request(server)
-                .post('/register')
+                .post('/auth/register')
                 .send({
                     firstName: 'firstNameTest',
                     lastName: 'lastNameTest',
@@ -51,9 +51,20 @@ describe('Auth Tests', () => {
                 })
         })
 
-        xit("Request with 'firstName' property empty", (done) => {
+        it("Request without 'firstName' property ", (done) => {
             chai.request(server)
-                .post('/register')
+                .post('/auth/register')
+                .send({ lastName: 'lastNameTest', email: 'test@test.test', password: 'passwordTest' })
+                .end((err, res) => {
+                    const error = res.body.errors[0].msg
+                    expect(error).to.equal('El nombre es obligatorio y debe ser un string.')
+                    done();
+                })
+        })
+
+        it("Request with 'firstName' property empty", (done) => {
+            chai.request(server)
+                .post('/auth/register')
                 .send({ firstName: '', lastName: 'lastNameTest', email: 'test@test.test', password: 'passwordTest' })
                 .end((err, res) => {
                     const error = res.body.errors[0].msg
@@ -62,9 +73,9 @@ describe('Auth Tests', () => {
                 })
         })
 
-        xit("Request with 'firstName' property value equal to number", (done) => {
+        it("Request with 'firstName' property value equal to number", (done) => {
             chai.request(server)
-                .post('/register')
+                .post('/auth/register')
                 .send({ firstName: 123, lastName: 'lastNameTest', email: 'test@test.test', password: 'passwordTest' })
                 .end((err, res) => {
                     const error = res.body.errors[0].msg
@@ -73,9 +84,9 @@ describe('Auth Tests', () => {
                 })
         })
 
-        xit("Request with 'firstName' property value equal to booblean", (done) => {
+        it("Request with 'firstName' property value equal to booblean", (done) => {
             chai.request(server)
-                .post('/register')
+                .post('/auth/register')
                 .send({ firstName: true, lastName: 'lastNameTest', email: 'test@test.test', password: 'passwordTest' })
                 .end((err, res) => {
                     const error = res.body.errors[0].msg
@@ -84,9 +95,9 @@ describe('Auth Tests', () => {
                 })
         })
 
-        xit("Request with 'firstName' property value equal to null", (done) => {
+        it("Request with 'firstName' property value equal to null", (done) => {
             chai.request(server)
-                .post('/register')
+                .post('/auth/register')
                 .send({ firstName: null, lastName: 'lastNameTest', email: 'test@test.test', password: 'passwordTest' })
                 .end((err, res) => {
                     const error = res.body.errors[0].msg
@@ -95,53 +106,142 @@ describe('Auth Tests', () => {
                 })
         })
 
-        xit("Request with 'lastName' property empty", (done) => {
+        it("Request without 'lastName' property", (done) => {
             chai.request(server)
-                .post('/register')
+                .post('/auth/register')
+                .send({ firstName: 'firstNameTest', email: 'test@test.test', password: 'passwordTest' })
+                .end((err, res) => {
+                    const error = res.body.errors[0].msg
+                    expect(error).to.equal('El apellido es obligatorio.')
+                    done();
+                })
+        })
+
+        it("Request with 'lastName' property empty", (done) => {
+            chai.request(server)
+                .post('/auth/register')
                 .send({ firstName: 'firstNameTest', lastName: '', email: 'test@test.test', password: 'passwordTest' })
                 .end((err, res) => {
                     const error = res.body.errors[0].msg
-                    expect(error).to.equal('El email es obligatorio.')
+                    expect(error).to.equal('El apellido es obligatorio.')
                     done();
                 })
-        })
-
-        xit("Request with 'lastName' property value equal to number", (done) => {
-            chai.request(server)
-                .post('/register')
-                .send({ firstName: 'firstNameTest', lastName: 123, email: 'test@test.test', password: 'passwordTest' })
-                .end((err, res) => {
-                    const error = res.body.errors[0].msg
-                    expect(error).to.equal('El email es obligatorio.')
-                    done();
-                })
-        })
-
-        xit("Request with 'lastname' property value equal to booblean", (done) => {
-            chai.request(server)
-                .post('/register')
-                .send({ firstName: 'firstNameTest', lastName: true, email: 'test@test.test', password: 'passwordTest' })
-                .end((err, res) => {
-                    const error = res.body.errors[0].msg
-                    expect(error).to.equal('El email es obligatorio.')
-                    done();
-                })
-        })
-
-        xit("Request with 'lastName' property value equal to null", (done) => {
-            chai.request(server)
-                .post('/register')
-                .send({ firstName: 'firstNameTest', lastName: null, email: 'test@test.test', password: 'passwordTest' })
-                .end((err, res) => {
-                    const error = res.body.errors[0].msg
-                    expect(error).to.equal('El email es obligatorio.')
-                    done();
-                })
-        })
-
+        }) 
         
+        
+        it("Request with 'email' property match with email format", (done) => {
+            chai.request(server)
+            .post('/auth/register')
+            .send({ firstName: 'firstNameTest', lastName: 'lastNameTest', email: 'testtest.test', password: 'passwordTest' })
+            .end((err, res) => {
+                const error = res.body.errors[0].msg
+                expect(error).to.equal('El correo no es valido.')
+                done();
+            })
+        })  
+        
+        it("Request with 'property' exist value", (done) => {
+            chai.request(server)
+                .post('/auth/register')
+                .send({ firstName: 'firstNameTest', lastName: 'lastNameTest', email: 'validvalues@test.test', password: 'passwordTest' })
+                .end((err, res) => {
+                    const error = res.body.errors[0].msg
+                    expect(error).to.equal('El email: validvalues@test.test ya esta registrado.')
+                    done();
+                })
+        })
+
+        it("Request with 'email' property match with email format", (done) => {
+            chai.request(server)
+            .post('/auth/register')
+            .send({ firstName: 'firstNameTest', lastName: 'lastNameTest', email: 'testtest.test', password: 'passwordTest' })
+            .end((err, res) => {
+                const error = res.body.errors[0].msg
+                expect(error).to.equal('El correo no es valido.')
+                done();
+            })
+        })  
+        
+        it("Request with 'password' value shorter than 6 characters", (done) => {
+            chai.request(server)
+                .post('/auth/register')
+                .send({ firstName: 'firstNameTest', lastName: 'lastNameTest', email: 'test@test.test', password: '1234' })
+                .end((err, res) => {
+                    const error = res.body.errors[0].msg
+                    expect(error).to.equal('El password debe ser de mas de 6 caracteres.')
+                    done();
+                })
+        })
+    })
+
+    // Post login method tests
+    describe('Post login Method', () => {
+
+        it("Should login a user", (done) => {
+            chai.request(server)
+                .post('/auth/login')
+                .send({
+                    email: 'juan@gmail.com',
+                    password: 'Juan1234'
+                })
+                .end((err, res) => {
+                    const msg = res.body.msg
+                    expect(msg).to.equal('Login Ok')
+                    done();
+                })
+        })
+
+        it("Request with user not found", (done) => {
+            chai.request(server)
+                .post('/auth/login')
+                .send({
+                    email: 'invalidemail@test.test',
+                    password: validValues.password
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(400)
+                    done();
+                })
+        })
+
+        it("Request with invalid password", (done) => {
+            chai.request(server)
+                .post('/auth/login')
+                .send({
+                    email: validValues.email,
+                    password: '1234'
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(409)
+                    done();
+                })
+        })
 
     })
 
+    // Get method tests
+    describe('Get authenticated Method', () => {
+
+        it('Request with user authenticated',(done) => {
+            chai.request(server)
+                .get('/auth/me')
+                .set('Authorization', `Bearer ${userToken}`)
+                .end((err, res) => {
+                    console.log(res)
+                    expect(res).to.have.status(200);
+                    done();
+                })
+        })
+
+        it('Request without user authenticated',(done) => {
+            chai.request(server)
+                .get('/auth/me')
+                .end((err, res) => {
+                    expect(res).to.have.status(403);
+                    done();
+                })
+        })
+
+    })
 
 })
